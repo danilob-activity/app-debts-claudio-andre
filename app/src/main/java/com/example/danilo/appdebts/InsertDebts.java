da38d582381d62d674f56ef7a4a207e0d97fd5d4
Claudio <andretraflagar@gmail.com>
2019-07-11 11:55:23 -0300

    7º commit

--- app/src/main/java/com/example/danilo/appdebts/InsertDebts.java
+++ app/src/main/java/com/example/danilo/appdebts/InsertDebts.java
@@ -1,18 +1,34 @@
 package com.example.danilo.appdebts;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.danilo.appdebts.classes.MainWindow;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class InsertDebts extends AppCompatActivity {
+
+
+    final Calendar myCalendar = Calendar.getInstance();
+    private String m_Text = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,9 +71,73 @@ public class InsertDebts extends AppCompatActivity {
                 startActivity( new Intent(this, MainWindow. class)); //O efeito ao ser pressionado do botão(no caso abre a activity)
                 finishAffinity(); //Método para matar a activity e não deixa-lá indexada na pilhagem
                 break;
+            case R.id.okmenu:
+                Log.d("ITEM MENU", "Menu"+ R.string.okmenu);
             default:break;
         }
         return true;
     }
+
+
+
+    EditText mEditTextDatapay = (EditText) findViewById(R.id.editTextDate);
+    EditText mEditTextDescription = (EditText) findViewById(R.id.editTextDescription);
+    EditText mEditText = (EditText) findViewById(R.id.editText);
+    Spinner mSpinner = (Spinner) findViewById(R.id.spinnerCategories);
+
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+
+    };
+
+    mEditTextDatapay.setOnClickListener(new OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            // TODO Auto-generated method stub
+            new DatePickerDialog(insertDebts.this, date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+        }
+    });
+
+    private void updateLabel() {
+        String myFormat = "dd/MM/yyyy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        mEditTextDatapay.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    builder.setTitle("Title");
+
+    // Set up the input
+    final EditText input = new EditText(this);
+    // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+    input.setInputType(InputType.TYPE_CLASS_TEXT);
+    builder.setView(input);
+
+    // Set up the buttons
+    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            m_Text = input.getText().toString();
+        }
+    });
+    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            dialog.cancel();
+        }
+    });
+
+    builder.show();
 
 }
